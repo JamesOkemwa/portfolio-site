c47ef1383c926ae5bc6bbbee459191d9e9f2ed38
lejames <okejames55@gmail.com>
2022-03-17 11:18:08 +0300

    contact info and footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillTwitterSquare, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
 import { FaGlobe } from "react-icons/fa"
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
