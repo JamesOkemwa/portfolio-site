681c9de63617fd9182fa0dbbcf276551eb23a4d9
lejames <okejames55@gmail.com>
2022-03-17 11:03:38 +0300

    about me section - timelines

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,7 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2018, text: 'Started my coding journey', },
+  { year: 2019, text: 'Worked as a freelance developer', },
+  { year: "2020 - Present", text: 'Started working for NatureSurf Ltd. as a Full Stack software engineer specializing in GIS projects.', },
 ];
